f93f22353fb0f49d7c81862a1076e3c9512b7faf
NOAH JAMES STEWART <njstewart2@wisc.edu>
2022-10-02 20:58:11 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,5 @@
 print('hello world')
-<<<<<<< HEAD
-
 print ('merging')
-=======
 print('adding a line')
-<<<<<<< HEAD
-<<<<<<< HEAD
 print('adding a line')
-=======
 print('now compiles')
->>>>>>> 05c9314518ca3f93fba66cd1b68bd195eb6df1c6
-=======
->>>>>>> 438c936732f6fb28dccae6c7ba279f704525f4dc
->>>>>>> 09ac082c87e91731b81d3f937539388d4e62313d
-print("Nosh Potosh")
