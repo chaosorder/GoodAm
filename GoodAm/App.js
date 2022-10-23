c2c1ea186b0dc1c00739cfc6953f90b78834279d
Uma Parhar <umaparhar@Umas-MacBook-Pro-2.local>
2022-10-23 17:54:01 -0500

    Android and IOS authentication

--- GoodAm/App.js
+++ GoodAm/App.js
@@ -1,15 +1,106 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
-
 import RootNavigator from './navigation/RootNavigator';
+import React, { useState, useEffect } from 'react';
+import { View, Text , StatusBar, StyleSheet, Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+createUser = () => {
+  auth()
+  .signInWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
+  .then(() => {
+    console.log('User account created & signed in!');
+  })
+  .catch(error => {
+    if (error.code === 'auth/email-already-in-use') {
+      console.log('That email address is already in use!');
+    }
+
+    if (error.code === 'auth/invalid-email') {
+      console.log('That email address is invalid!');
+    }
+
+    console.error(error);
+  });
+}
+
+logOff = () => {
+  auth()
+  .signOut()
+  .then(() => console.log('User signed out!'));
+}
+
+GoogleSignin.configure({
+  webClientId: '541770806381-3prnmcl555bfnna1ffrgcl3d30c74g6g.apps.googleusercontent.com',
+});
+
+onGoogleButtonPress = async () => {
+  // Check if your device supports Google Play
+  await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+  // Get the users ID token
+  const { idToken } = await GoogleSignin.signIn();
+
+  // Create a Google credential with the token
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+  // Sign-in the user with the credential
+  return auth().signInWithCredential(googleCredential);
+}
 
 const App = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text>Login</Text>
+        <Button title = "Create User" onPress = {this.createUser} />
+        <Button
+        title="Google Sign-In"
+        onPress={() => this.onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+        />
+      </>
+    );
+  }
+
   return (
     <>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text>Welcome {user.email}</Text>
+      <Button title = "Log off" onPress={this.logOff} />
       <StatusBar />
       <RootNavigator />
     </>
   );
-};
+}
 
 export default App;
