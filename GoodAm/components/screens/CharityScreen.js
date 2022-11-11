84693672ed14ef42453acc42e25ac3fafe4b3fa9
NOAH JAMES STEWART <njstewart2@wisc.edu>
2022-11-11 12:46:55 -0600

    added basic android notification

--- GoodAm/components/screens/CharityScreen.js
+++ GoodAm/components/screens/CharityScreen.js
@@ -1,15 +1,37 @@
 /*
 Landing screen for the charity tab
 */
-import React from 'react';
+import React, { useEffect } from 'react';
 import {View, Text} from 'react-native';
 import {StyleSheet} from 'react-native';
 import {COLORS} from '../../assets/colors';
+import {ReusableButton} from '../ReusableButton'
+import PushNotification from 'react-native-push-notification';
+
+const createChannels = () => {
+  PushNotification.createChannel({
+    channelId: 'test-channel',
+    channelName: 'Test Channel'
+  });
+}
+
+const handleNotification = () => {
+  PushNotification.localNotification({
+    channelId: 'test-channel',
+    title: 'Test Alarm',
+    message: 'This is a test alarm.'
+  })
+}
 
 const CharityScreen = () => {
+  useEffect(() => {
+    createChannels();
+  }, []);
+
   return (
     <View style={style.container}>
-      <Text style={style.font}>Charity</Text>
+      <ReusableButton title='Click me!' onPress={handleNotification}></ReusableButton>
+      {/* <Text style={style.font}>Charity</Text> */}
     </View>
   );
 };
