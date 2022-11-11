ac5d96614453ee9c0c0118c5155d0f68a27d38dc
ojassethi <52195419+chaosorder@users.noreply.github.com>
2022-11-10 21:19:19 -0600

    implemented alarm list

--- GoodAm/components/screens/AlarmScreen.js
+++ GoodAm/components/screens/AlarmScreen.js
@@ -3,11 +3,12 @@ Landing screen for the alarms tab, will house alarms
 to be toggled and the ability to create alarms.
 */
 import React, {useState} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, ScrollView, TouchableOpacity} from 'react-native';
 import {StyleSheet} from 'react-native';
 import {COLORS} from '../../assets/colors'; //our colors for the project, just call this everytime
 import {ReusableButton} from '../ReusableButton';
 import {ReusableModal} from '../ReusableModal';
+import {Alarm} from '../Alarm'
 import {usersCollection} from '../alarm/usersCollection';
 import firestore from '@react-native-firebase/firestore';
 
@@ -18,6 +19,22 @@ const AlarmScreen = () => {
     setIsModalVisible(() => !isModalVisible);
     setName('');
   };
+  const [alarms, setAlarms] = useState({
+    0: {time: "10:10 PM", active: true},
+    1: {time: "10:15 PM", active: true},
+    2: {time: "10:20 PM", active: false},
+    3: {time: "10:25 PM", active: true},
+    4: {time: "10:30 PM", active: false},
+    5: {time: "10:35 PM", active: true},
+    6: {time: "6:00 AM", active: true},
+  })
+
+  const toggleAlarm = (key) => {
+    console.log(key);
+    newAlarms = {...alarms};
+    newAlarms[key] = {time: alarms[key].time, active: !alarms[key].active}
+    setAlarms(newAlarms);
+  }
 
   const addName = text => {
     //temporary function to add user input to database
@@ -29,12 +46,26 @@ const AlarmScreen = () => {
       });
   };
 
+  const buildAlarms = () => {
+    let alarmComponents = [];
+    for (let key in alarms) {
+      alarmComponents.push(
+        <Alarm key={key} toggle={toggleAlarm} alarmId={key} data={alarms[key]} />);
+    }
+    return alarmComponents;
+  }
+
   return (
-    <View style={style.container}>
-      <Text style={style.font}>Your Alarms</Text>
-      <ReusableButton //create new alarm button, customizable
-        title="Create new alarm"
-        onPress={handleModal}></ReusableButton>
+    <ScrollView style={style.container}>
+      <View style={style.header}>
+        <Text style={style.font}>Your Alarms</Text>
+        <TouchableOpacity
+          style={style.addButton}
+          onPress={handleModal}>
+          <Text style={style.buttonText}>+ Alarm</Text>
+        </TouchableOpacity>
+      </View>
+      {buildAlarms()}
       <ReusableModal isVisible={isModalVisible}>
         <ReusableModal.Container>
           <ReusableModal.Header title="Create Alarm" />
@@ -62,19 +93,25 @@ const AlarmScreen = () => {
           </ReusableModal.Footer>
         </ReusableModal.Container>
       </ReusableModal>
-    </View>
+    </ScrollView>
   );
 };
 
 const style = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "100%",
     backgroundColor: COLORS.naplesYellow,
   },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end"
+  },
   font: {
+    marginTop: 80,
+    marginBottom: 20,
+    marginLeft: 20,
     fontSize: 20,
     color: COLORS.indigoDye,
   },
@@ -85,6 +122,20 @@ const style = StyleSheet.create({
     borderWidth: 3,
     color: COLORS.lemonChiffon,
   },
+  addButton: {
+    marginRight: 20,
+    marginBottom: 12,
+    paddingVertical: 10,
+    borderRadius: 25,
+    width: '25%',
+    alignItems: 'center',
+    backgroundColor: COLORS.indigoDye,
+  },
+  buttonText: {
+    fontWeight: '500',
+    fontSize: 16,
+    color: COLORS.lemonChiffon,
+  }
 });
 
 export default AlarmScreen;
