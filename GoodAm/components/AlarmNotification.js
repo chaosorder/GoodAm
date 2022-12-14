6aca6b1b41c582a949d71f9c4b4bddaa1e9bb9c0
NOAH JAMES STEWART <njstewart2@wisc.edu>
2022-12-14 14:01:21 -0600

    added more comments

--- GoodAm/components/AlarmNotification.js
+++ GoodAm/components/AlarmNotification.js
@@ -1,12 +1,20 @@
-import notifee, { TriggerType, AndroidImportance, AndroidCategory, EventType} from '@notifee/react-native';
-import React from 'react';
-import SnoozeScreen from './screens/SnoozeScreen';
+import notifee, { TriggerType, 
+  AndroidImportance, 
+  AndroidCategory, 
+  EventType } from '@notifee/react-native';
 
-let alarmState;
+let alarmState; // Keeps track of whether or not an alarm is currently sounding.
 
-/* Creates the initial notification channel if one does not already exist */
+/**
+* Creates the Android notification channel for alarms and passes through
+* a state handler function to allow tracking of whether or not an alarm is 
+* currently sounding. 
+* 
+* @param alarming          the alarm state handler function
+*/ 
 export const createInitialNotificationChannel = async (alarming) => {
   alarmState = alarming;
+  
   await notifee.createChannel({
     id: 'gam',
     name: 'Alarm Channel',
@@ -20,8 +28,14 @@ export const createInitialNotificationChannel = async (alarming) => {
   });
 };
 
-function parseTime(timeString) {	
-	if (timeString == '') return null;
+/**
+* Creates and returns a Date object from a database alarm entry's time string.
+* 
+* @param {string} timeString          alarm entry's alarm time
+* @returns                            date object set to desired time       
+*/
+const parseTime = (timeString) => {	
+  if (timeString == '') return null;
 	
 	var time = timeString.match(/(\d+)(:(\d\d))?\s*(p?)/i);	
 	if (time == null) return null;
@@ -32,7 +46,8 @@ function parseTime(timeString) {
 	}
 	else {
 		hours += (hours < 12 && time[4])? 12 : 0;
-	}	
+	}
+
 	var d = new Date();    	    	
 	d.setHours(hours);
 	d.setMinutes(parseInt(time[3],10) || 0);
@@ -40,10 +55,13 @@ function parseTime(timeString) {
 	return d.getTime();
 }
 
-/* Schedule a notification to be displayed at a specific time */
+/**
+* Schedules an alarm notification to be displayed at a specific time.
+* 
+* @param {string} alarmId          id of alarm from the database
+* @param {string} triggerTime      time for notification to be displayed
+*/
 export const scheduleNotification = async (alarmId, triggerTime) => {
-  // TODO:: Add more flexible scheduling (Recurring, which days of the week, etc.)
-  console.log(parseTime(triggerTime));
   const notificationTrigger = {
     type: TriggerType.TIMESTAMP,
     timestamp: parseTime(triggerTime),
@@ -54,7 +72,7 @@ export const scheduleNotification = async (alarmId, triggerTime) => {
     title: 'It\'s time to wake up!',
     android: {
       channelId: 'gam',
-      smallIcon: 'ic_small_icon',
+      smallIcon: 'ic_small_icon', // Custom icon
       color: '#0c4767',
       ongoing: true, // User must interact with notification to dismiss
       vibrationPattern: [180, 121, 134, 300, 289, 123, 134, 300, 621, 121, 
@@ -86,46 +104,69 @@ export const scheduleNotification = async (alarmId, triggerTime) => {
   }, notificationTrigger);
 };
 
-/* Handle what happens if a notification event occurs while the application is in the background */
+/**
+* Cancels notification if notification button was pressed and app was in the
+* background.
+*/
 export const handleNotificationBackgroundEvent = async () => {
   notifee.onBackgroundEvent(async ({ type, detail }) => {
+    // Snooze button pressed
     if (type === EventType.ACTION_PRESS && detail.pressAction.id == 'snooze') {
-      // TODO:: Add routing to snooze page
       await notifee.cancelNotification(detail.notification.id);
     }
+    // Awake button pressed
     else if (type == EventType.ACTION_PRESS && detail.pressAction.id == 'awake') {
       await notifee.cancelNotification(detail.notification.id);
     }
   });
 }
 
-/* Handle what happens if a notification event occurs while the application is in the foreground */
+/**
+* Cancels notification if a notification event occurs while the app is in the
+* foreground. Sets the alarm state to true if snooze is selected so app can be
+* rerouted to the snooze screen.
+*/
 export const handleNotificationForegroundEvent = () => {
   notifee.onForegroundEvent(({ type, detail }) => {
+    // Snooze button pressed
     if (type === EventType.ACTION_PRESS && detail.pressAction.id == 'snooze') {
-      // TODO:: Add routing to snooze page
       alarmState(true);
       notifee.cancelNotification(detail.notification.id);
     }
+    // Awake button pressed
     else if (type == EventType.ACTION_PRESS && detail.pressAction.id == 'awake'){
       notifee.cancelNotification(detail.notification.id);
     }
   });
 }
 
-/* Cancel the notification that triggered that app to open */
+/**
+* Cancels the notification that triggered the app to open.
+*/
 export const cancelCurrentNotification = async () => {
   // Get notification that triggered app to open
   const currentNotification = await notifee.getDisplayedNotifications();
-  if (currentNotification.length)
+  
+  if (isAlarming)
     notifee.cancelNotification(currentNotification[0].notification.id);
+  
   alarmState(false);
 }
 
+/**
+ * Cancel a scheduled notification with a specific id.
+ * 
+ * @param {*} alarmId         alarm's database entry id
+ */
 export const cancelScheduledNotification = (alarmId) => {
   notifee.cancelNotification(alarmId);
 }
 
+/**
+ * Checks if a alarm is currently alarming.
+ * 
+ * @returns         true if alarming, false otherwise
+ */
 export const isAlarming = async () => {
   if (!(await notifee.getDisplayedNotifications()).length)
     return false;
--- GoodAm/components/screens/SnoozeScreen.js
+++ GoodAm/components/screens/SnoozeScreen.js
@@ -5,27 +5,37 @@ import {COLORS} from '../../assets/colors';
 import {ReusableButton} from '../ReusableButton';
 import {cancelCurrentNotification} from '../AlarmNotification';
 
+/**
+ * Cancels alarm notification and directs to charity donation page.
+ */
 const snoozeClicked = () => {
   cancelCurrentNotification();
   Linking.openURL('https://www.every.org/givedirectly#/donate/card');
 };
 
+/**
+ * Cancels alarm notification.
+ */
 const awakeClicked = () => {
   cancelCurrentNotification();
-  //BackHandler.exitApp();
 }
 
+/**
+ * Screen the user is taken to if their alarm goes off.
+ * 
+ * @returns         snooze screen page
+ */
 const SnoozeScreen = props => {
   return (
     <View style={style.container}>
       <Text style={style.font}>Time to wakeup!</Text>
       <ReusableButton  title={'I\'m awake!'} onPress={awakeClicked} />
-      <ReusableButton title={'Just 15 more minutes...'} 
-        onPress={snoozeClicked} />
+      <ReusableButton title={'Just 15 more minutes...'} onPress={snoozeClicked} />
     </View>
   );
 };
 
+// Create style sheet for snooze screen
 const style = StyleSheet.create({
   container: {
     flex: 1,
